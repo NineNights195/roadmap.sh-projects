3a9ef66c52bba505fd85516e64472da3b92cd873
NineNights195 <ninenights19534@gmail.com>
2026-04-22 18:45:20 +0700

    rebuild the script

--- frontend/12-restricted-textarea/script.js
+++ frontend/12-restricted-textarea/script.js
@@ -3,14 +3,13 @@ const textCount = document.querySelector(".text-count");
 const maxCharacters = 250;
 
 const updateCounter = () => {
-    if (textarea.value.length > maxCharacters) {
+    if (textarea.value.length >= maxCharacters) {
         textarea.value = textarea.value.slice(0, maxCharacters);
     }
-
     const used = textarea.value.length;
     textCount.textContent = `${used}/${maxCharacters}`;
-    textarea.classList.toggle("restricted", used >= maxCharacters);
-    textCount.classList.toggle("restricted", used >= maxCharacters);
+    textarea.classList.toggle('restricted' , used >= maxCharacters);
+    textCount.classList.toggle('restricted' , used >= maxCharacters);
 };
 
 textarea.addEventListener("input", updateCounter);
