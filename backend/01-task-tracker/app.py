a10c960a1b53a1b0c7b29c42a884a83ff6a5d063
NineNights195 <ninenights19534@gmail.com>
2026-06-04 18:02:06 +0700

    update task tracker: make add function works, fix list function if it's none to show

--- backend/01-task-tracker/app.py
+++ backend/01-task-tracker/app.py
@@ -4,10 +4,25 @@ import json
 
 app = typer.Typer()
 
+# Get tasks list from tasks.json (If there's none, create a new one)
+try:
+    with open('tasks.json', 'r') as f:
+        data = json.load(f)
+except FileNotFoundError:
+    data = {"tasks": []}
+    with open('tasks.json', 'w') as f:
+        json.dump(data, f, indent=4)
+tasks_list = data.get("tasks", [])
+
 # Adding a new task
 @app.command()
 def add(description: str):
+
+    existing_ids = {t.get("id", 0) for t in tasks_list}
     task_id = 1
+    while task_id in existing_ids:
+        task_id += 1
+
     task = {
         "id": task_id,
         "description": description,
@@ -15,6 +30,13 @@ def add(description: str):
         "createdAt": datetime.datetime.now().isoformat(),
         "updatedAt": None,
     }
+
+    tasks_list.append(task)
+    data["tasks"] = tasks_list
+
+    with open('tasks.json', 'w') as f:
+        json.dump(data, f, indent=4)
+
     print(f"Task added successfully (ID: {task_id})")
 
 # Updating tasks
@@ -31,10 +53,7 @@ def delete(id: int):
 # Listing all tasks
 @app.command()
 def list():
-    with open('tasks.json', 'r') as f:
-        tasks = json.load(f)
-        for task in tasks['tasks']:
-            print(task)
+    print(json.dumps(data['tasks'], indent=2))
 
 # Run "task-cli"
 def cli() -> None:
